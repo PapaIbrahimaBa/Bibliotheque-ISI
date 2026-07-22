cbe2f0183aa22496497a3004f179bfefdd35456a
PIPO <bapipo123@gmail.com>
2026-07-22 10:38:44 +0000

    Refactor categories.h header guard and structure

--- Projet_C/include/categories.h
+++ Projet_C/include/categories.h
@@ -1,6 +1,19 @@
-#ifndef CATEGORIES_H_INCLUDED
-#define CATEGORIES_H_INCLUDED
+#ifndef CATEGORIE_H
+#define CATEGORIE_H
 
+typedef struct {
+    int id;
+    char libelle[50];
+    char description[200];
+    char dateCreation[20];
+} Categorie;
 
+int genererIdCategorie();
+void menuCategorie();
+void ajouterCategorie();
+void afficherCategories();
+void rechercherCategorie();
+void modifierCategorie();
+void supprimerCategorie();
 
 #endif
