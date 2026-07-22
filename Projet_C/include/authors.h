6f1b870d124214cb817de0c92e492afacbd01700
PIPO <bapipo123@gmail.com>
2026-07-22 10:37:12 +0000

    Define auteur structure and author management functions

--- Projet_C/include/authors.h
+++ Projet_C/include/authors.h
@@ -1,6 +1,19 @@
-#ifndef AUTHORS_H_INCLUDED
-#define AUTHORS_H_INCLUDED
-
-
-
+#ifndef AUTEUR_H
+#define AUTEUR_H
+typedef struct {
+int id;
+char nomComplet[100];
+char nationalite[100];
+char dateNaissance[100];
+char biographie[100];
+int nbLivres;
+}auteur;
+void menuAuteur();
+void ajouterAuteur();
+void afficherAuteur();
+void rechercherAuteur();
+void modifierAuteur();
+void supprimerAuteur();
+int genererIdAuteur();
+int auteurPossedeLivre(int idAuteur);
 #endif
